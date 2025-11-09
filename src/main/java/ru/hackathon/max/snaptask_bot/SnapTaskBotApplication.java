d07629d972242ffaec1417a5365316126ba1a2c3
Grigoriy <quracau@gmail.com>
2025-11-09 16:26:59 +0300

    initial commit

---
+++ src/main/java/ru/hackathon/max/snaptask_bot/SnapTaskBotApplication.java
@@ -0,0 +1,12 @@
+package ru.hackathon.max.snaptask_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapTaskBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapTaskBotApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/hackathon/max/snaptask_bot/SnapTaskBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hackathon.max.snaptask_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapTaskBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
