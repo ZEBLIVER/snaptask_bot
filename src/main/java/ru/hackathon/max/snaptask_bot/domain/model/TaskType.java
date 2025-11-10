682a42cc0e02efd857f20e4b48320fb8d5d29ccb
Grigoriy <quracau@gmail.com>
2025-11-10 17:07:05 +0300

    add taskType enum

---
+++ src/main/java/ru/hackathon/max/snaptask_bot/domain/model/TaskType.java
@@ -0,0 +1,9 @@
+package ru.hackathon.max.snaptask_bot.domain.model;
+
+public enum TaskType {
+    ACADEMIC, // Учеба (отчет, курсовая)
+    WORK, // Работа (бизнес-задача)
+    PERSONAL, // Личные дела (поход к врачу, оплата счетов)
+    HABIT, // Привычка
+    UNDEFINED
+}
