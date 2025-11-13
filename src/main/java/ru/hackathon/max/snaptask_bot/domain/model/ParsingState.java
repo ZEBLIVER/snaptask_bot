864a4952fef3c789944f127536a8bec15dcc0e9b
Grigoriy <quracau@gmail.com>
2025-11-13 23:31:52 +0300

    feat: add ZoneId varible for ParsingState model

--- src/main/java/ru/hackathon/max/snaptask_bot/domain/model/ParsingState.java
+++ src/main/java/ru/hackathon/max/snaptask_bot/domain/model/ParsingState.java
@@ -3,6 +3,7 @@ package ru.hackathon.max.snaptask_bot.domain.model;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Optional;
 
 @Getter
@@ -10,21 +11,24 @@ public class ParsingState {
     private String remainingText;
     private final Optional<LocalDateTime> deadline;
     private final Optional<String> recurrenceRule;
+    private final ZoneId userZoneId;
 
-    public ParsingState(String initialText) {
+    public ParsingState(String initialText, ZoneId userZoneId) {
         this.remainingText = initialText.toLowerCase().trim();
         this.deadline = Optional.empty();
         this.recurrenceRule = Optional.empty();
+        this.userZoneId = userZoneId; // <--- СОХРАНЕНИЕ
     }
 
-    private ParsingState(String remainingText, Optional<LocalDateTime> deadline, Optional<String> recurrenceRule) {
+    private ParsingState(String remainingText, Optional<LocalDateTime> deadline, Optional<String> recurrenceRule, ZoneId userZoneId) {
         this.remainingText = remainingText;
         this.deadline = deadline;
         this.recurrenceRule = recurrenceRule;
+        this.userZoneId = userZoneId;
     }
 
     public ParsingState update(String newRemainingText, Optional<LocalDateTime> newDeadline, Optional<String> newRecurrenceRule) {
-        return new ParsingState(newRemainingText, newDeadline, newRecurrenceRule);
+        return new ParsingState(newRemainingText, newDeadline, newRecurrenceRule, this.userZoneId);
     }
 
     public boolean isDeadlineSet() {
